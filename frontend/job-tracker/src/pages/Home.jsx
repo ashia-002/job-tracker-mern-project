e1e3019ef7be6f29550889b0636e79cbf11f1353
Ashia Sultana <ashia.sultana.maisha@gmail.com>
2024-12-29 00:52:11 +0600

    Added component Home page(Home.jsx).

---
+++ frontend/job-tracker/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import HeroSection from "../components/HeroSection";
+import ExploreSection from "../components/ExploreSection";
+import TableSection from "../components/TableSection";
+import AboutSection from "../components/AboutSection";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+      <ExploreSection/>
+      <TableSection/>
+      <AboutSection/>
+    </div>
+  );
+};
+
+export default Home;
